ed467ffed7d465f960f24e28486fb6359af579bf
Slaughter-test <thedarkestbreakdown@gmail.com>
2021-11-07 14:47:22 +0300

    first commit

---
+++ GBMapLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBMapLesson
+//
+//  Created by Юрий Егоров on 07.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
