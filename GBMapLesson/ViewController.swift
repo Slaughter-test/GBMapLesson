1a8b2c1d422f29637093fdd85ce1e94abfb7f204
Slaughter-test <thedarkestbreakdown@gmail.com>
2021-11-07 18:14:46 +0300

    .

--- GBMapLesson/ViewController.swift
+++ GBMapLesson/ViewController.swift
@@ -10,6 +10,7 @@ import GoogleMaps
 
 class ViewController: UIViewController {
     
+    //MARK: - Variables
     var mapView: GMSMapView!
     var locationManager: CLLocationManager?
     var currentLocation = CLLocationCoordinate2D(latitude: 59.939095, longitude: 30.315868)
@@ -20,52 +21,10 @@ class ViewController: UIViewController {
         setupLocationManager()
         setupCamera(location: currentLocation)
         updateCurrentLocation(self)
+        constraints()
     }
     
-    private func setupViews() {
-        
-        //mapView
-        mapView = GMSMapView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - 50))
-        
-        view.addSubview(mapView)
-        mapView.translatesAutoresizingMaskIntoConstraints = false
-        let constraints = [
-            mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            mapView.widthAnchor.constraint(equalTo: view.widthAnchor),
-            mapView.heightAnchor.constraint(equalTo: view.heightAnchor)
-        ]
-        NSLayoutConstraint.activate(constraints)
-        
-        //navigationBar
-        
-        let navigationItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(updateCurrentLocation(_:)))
-        navigationController?.navigationBar.backgroundColor = .white
-        navigationController?.navigationBar.isTranslucent = false
-        self.navigationItem.rightBarButtonItem = navigationItem
-        
-        
-    }
-    
-    private func setupLocationManager() {
-        locationManager = CLLocationManager()
-        locationManager?.requestWhenInUseAuthorization()
-        locationManager?.delegate = self
-    }
-    
-    private func updateCamera(location: CLLocationCoordinate2D) {
-        mapView.animate(toLocation: location)
-    }
-    
-    private func createMark(location: CLLocationCoordinate2D) {
-        let marker = GMSMarker(position: location)
-        marker.map = mapView
-    }
-    
-    private func setupCamera(location: CLLocationCoordinate2D) {
-        mapView.camera = GMSCameraPosition.camera(withTarget: location, zoom:10)
-    }
-    
+    //MARK: - User Actions
     @objc
     func updateCurrentLocation(_ sender: Any) {
         locationManager?.requestLocation()
@@ -75,6 +34,49 @@ class ViewController: UIViewController {
         currentLocation = location
         updateCamera(location: location)
         createMark(location: location)
+    }
+    
+    //MARK: - View Actions
+    private func updateCamera(location: CLLocationCoordinate2D) {
+        mapView.animate(toLocation: location)
+    }
+    
+    private func createMark(location: CLLocationCoordinate2D) {
+        let marker = GMSMarker(position: location)
+        marker.map = mapView
+    }
+    
+    //MARK: - Configs
+    private func setupCamera(location: CLLocationCoordinate2D) {
+        mapView.camera = GMSCameraPosition.camera(withTarget: location, zoom:10)
+    }
+    
+    private func constraints() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        let constraints = [
+            mapView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            mapView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            mapView.widthAnchor.constraint(equalTo: view.widthAnchor),
+            mapView.heightAnchor.constraint(equalTo: view.heightAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    private func setupViews() {
+        mapView = GMSMapView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height - 50))
+        view.addSubview(mapView)
+        
+        // navigationBar
+        let navigationItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(updateCurrentLocation(_:)))
+        navigationController?.navigationBar.backgroundColor = .white
+        navigationController?.navigationBar.isTranslucent = false
+        self.navigationItem.rightBarButtonItem = navigationItem
+    }
+    
+    private func setupLocationManager() {
+        locationManager = CLLocationManager()
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.delegate = self
     }
     
 }
